969abb9552d5dd1962a3245d95883bc2f4f117aa
Alen <cnxysoft@126.com>
2026-05-28 01:12:19 +0800

    feat(weibo): 新增移动端 API 响应 proto 定义及转换方法
    - 新增 WeiboMobileProfileResponse/WeiboMobileCardsResponse 及其嵌套结构
    - 修复 darwin_tags/safe_tags/page_info 等字段类型不匹配问题
    - 新增 MobileMblog -> Card 转换逻辑，支持转发微博完整解析
    - 新增 pics 数组到 pic_infos map 的转换函数
    - apiContainerGetIndexProfileAPI/CardsAPI 改用新类型并通过转换方法保持兼容

--- lsp/weibo/containerGetIndex.go
+++ lsp/weibo/containerGetIndex.go
@@ -1,11 +1,14 @@
 package weibo
 
 import (
+	stdjson "encoding/json"
 	"fmt"
 	"math/rand"
 	"strconv"
 	"strings"
 	"time"
+
+	structpb "google.golang.org/protobuf/types/known/structpb"
 
 	"github.com/cnxysoft/DDBOT-WSa/lsp/cfg"
 	"github.com/cnxysoft/DDBOT-WSa/proxy_pool"
@@ -72,12 +75,12 @@ func apiContainerGetIndexProfileAPI(uid int64) (*ApiContainerGetIndexProfileResp
 	}
 	apiURL := fmt.Sprintf("%s/api/Weibo/GetMobileProfile?uid=%d", baseURL, uid)
 
-	profileResp := new(ApiContainerGetIndexProfileResponse)
-	err := requests.Get(apiURL, nil, &profileResp)
+	mobileResp := new(WeiboMobileProfileResponse)
+	err := requests.Get(apiURL, nil, &mobileResp)
 	if err != nil {
 		return nil, err
 	}
-	return profileResp, nil
+	return mobileResp.ToProfileResponse(), nil
 }
 
 func ApiContainerGetIndexCards(uid int64) (*ApiContainerGetIndexCardsResponse, error) {
@@ -162,12 +165,12 @@ func apiContainerGetIndexCardsAPI(uid int64) (*ApiContainerGetIndexCardsResponse
 	}
 	apiURL := fmt.Sprintf("%s/api/Weibo/GetMobileCards?uid=%d", baseURL, uid)
 
-	cardsResp := new(ApiContainerGetIndexCardsResponse)
-	err := requests.Get(apiURL, nil, &cardsResp)
+	mobileResp := new(WeiboMobileCardsResponse)
+	err := requests.Get(apiURL, nil, &mobileResp)
 	if err != nil {
 		return nil, err
 	}
-	return cardsResp, nil
+	return mobileResp.ToCardsResponse(), nil
 }
 
 func apiContainerGetIndexCardsGuest(uid int64) (*ApiContainerGetIndexCardsResponse, error) {
@@ -257,6 +260,217 @@ func (r *apiContainerGetIndexGuestProfileResponse) ToProfileResponse() *ApiConta
 	}
 	resp.Data = data
 	return resp
+}
+
+// ToProfileResponse 将移动端用户资料转换为通用 Profile 响应格式
+func (r *WeiboMobileProfileResponse) ToProfileResponse() *ApiContainerGetIndexProfileResponse {
+	resp := &ApiContainerGetIndexProfileResponse{Ok: r.Ok}
+	if r.Data == nil {
+		return resp
+	}
+	data := &ApiContainerGetIndexProfileResponse_Data{}
+	if r.Data.UserInfo != nil {
+		data.User = &ApiContainerGetIndexProfileResponse_Data_UserInfo{
+			Id:               r.Data.UserInfo.Id,
+			ScreenName:       r.Data.UserInfo.ScreenName,
+			ProfileImageUrl:  r.Data.UserInfo.ProfileImageUrl,
+			ProfileUrl:       r.Data.UserInfo.ProfileUrl,
+		}
+	}
+	resp.Data = data
+	return resp
+}
+
+// ToCardsResponse 将移动端卡片响应转换为通用卡片响应格式
+func (r *WeiboMobileCardsResponse) ToCardsResponse() *ApiContainerGetIndexCardsResponse {
+	resp := &ApiContainerGetIndexCardsResponse{Ok: r.Ok}
+	if r.Data == nil {
+		return resp
+	}
+	resp.Data = &ApiContainerGetIndexCardsResponse_Data{
+		SinceId: strconv.FormatInt(r.Data.CardlistInfo.SinceId, 10),
+		List:    r.Data.CardsToCardList(),
+	}
+	return resp
+}
+
+// CardsToCardList 将 MobileCard 列表转换为 Card 列表
+func (r *WeiboMobileCardsResponse_Data) CardsToCardList() []*Card {
+	var list []*Card
+	for _, mc := range r.Cards {
+		if mc.Mblog != nil {
+			card := mc.ToCard()
+			list = append(list, card)
+		}
+	}
+	return list
+}
+
+// ToCard 将 MobileCard 转换为 Card
+func (r *MobileCard) ToCard() *Card {
+	if r.Mblog == nil {
+		return nil
+	}
+	return r.Mblog.ToCard()
+}
+
+// ToCard 将 MobileMblog 转换为 Card
+func (r *MobileMblog) ToCard() *Card {
+	if r == nil {
+		return nil
+	}
+	card := &Card{
+		Id:        int64StrToInt64(r.Id),
+		Mid:       r.Mid,
+		Text:      r.Text,
+		TextLength: r.TextLength,
+		RawText:   r.RawText,
+		CreatedAt: r.CreatedAt,
+		User:      r.User.ToUserInfo(),
+		Mblogid:   r.Mblogid,
+		Mblogtype: CardType(r.Mblogtype),
+		PicIds:    r.PicIds,
+		PageInfo:  toCardPageInfo(r.PageInfo),
+	}
+	if r.Visible != nil {
+		card.Visible = &Card_Visible{
+			Type:   r.Visible.Type,
+			ListId: r.Visible.ListId,
+		}
+	}
+	if r.RetweetedStatus != nil {
+		card.RetweetedStatus = r.RetweetedStatus.ToCard()
+	}
+	// 处理图片：优先用 pic_infos，否则从 pics 构造
+	if len(r.PicInfos) > 0 {
+		card.PicInfos = r.PicInfos
+	} else if r.Pics != nil {
+		card.PicInfos = extractPicsToPicInfos(r.Pics)
+	}
+	return card
+}
+
+// extractPicsToPicInfos 将移动端 pics 数组转换为 Card.PicInfo map
+func extractPicsToPicInfos(v *structpb.Value) map[string]*Card_PicInfo {
+	if v == nil {
+		return nil
+	}
+	data, err := v.MarshalJSON()
+	if err != nil {
+		return nil
+	}
+	var pics []struct {
+		Pid  string `json:"pid"`
+		Url  string `json:"url"`
+		Size string `json:"size"`
+		Large *struct {
+			Url string `json:"url"`
+		} `json:"large"`
+	}
+	if err := stdjson.Unmarshal(data, &pics); err != nil {
+		return nil
+	}
+	result := make(map[string]*Card_PicInfo)
+	for _, pic := range pics {
+		info := &Card_PicInfo{
+			Original: &Card_PicVariant{Url: pic.Url},
+		}
+		if pic.Large != nil {
+			info.Large = &Card_PicVariant{Url: pic.Large.Url}
+		}
+		result[pic.Pid] = info
+	}
+	return result
+}
+
+// toCardPageInfo 将 google.protobuf.Value 转换为 *Card_PageInfo
+func toCardPageInfo(v *structpb.Value) *Card_PageInfo {
+	if v == nil {
+		return nil
+	}
+	data, err := v.MarshalJSON()
+	if err != nil {
+		return nil
+	}
+	var pi struct {
+		Type       interface{} `json:"type"`
+		PageId     string      `json:"page_id"`
+		ObjectId   string      `json:"object_id"`
+		ObjectType interface{} `json:"object_type"`
+		Content1   string      `json:"content1"`
+		Content2   string      `json:"content2"`
+		PagePic    *struct {
+			Url string `json:"url"`
+		} `json:"page_pic"`
+		PageTitle string `json:"page_title"`
+		ShortUrl  string `json:"short_url"`
+		PageUrl   string `json:"page_url"`
+		ActStatus int32  `json:"act_status"`
+	}
+	if err := stdjson.Unmarshal(data, &pi); err != nil {
+		return nil
+	}
+	pageInfo := &Card_PageInfo{}
+	if pi.PageId != "" {
+		pageInfo.PageId = pi.PageId
+	}
+	if pi.ObjectId != "" {
+		pageInfo.ObjectId = pi.ObjectId
+	}
+	if pi.PagePic != nil && pi.PagePic.Url != "" {
+		pageInfo.PagePic = pi.PagePic.Url
+	}
+	if pi.PageTitle != "" {
+		pageInfo.PageTitle = pi.PageTitle
+	}
+	if pi.Content1 != "" {
+		pageInfo.Content1 = pi.Content1
+	}
+	if pi.Content2 != "" {
+		pageInfo.Content2 = pi.Content2
+	}
+	if pi.ShortUrl != "" {
+		pageInfo.ShortUrl = pi.ShortUrl
+	}
+	if pi.PageUrl != "" {
+		pageInfo.ShortUrl = pi.PageUrl
+	}
+	if pi.ActStatus != 0 {
+		pageInfo.ActStatus = pi.ActStatus
+	}
+	// type 字段用 Value 包装
+	if pi.Type != nil {
+		pageInfo.Type, _ = structpb.NewValue(pi.Type)
+	}
+	// object_type 可能是 string 或 int，转换为 string
+	if pi.ObjectType != nil {
+		switch v := pi.ObjectType.(type) {
+		case string:
+			pageInfo.ObjectType = v
+		case float64:
+			pageInfo.ObjectType = strconv.FormatFloat(v, 'f', -1, 64)
+		}
+	}
+	return pageInfo
+}
+
+// ToUserInfo 将 WeiboMobileUserInfo 转换为 ApiContainerGetIndexProfileResponse_Data_UserInfo
+func (r *WeiboMobileUserInfo) ToUserInfo() *ApiContainerGetIndexProfileResponse_Data_UserInfo {
+	if r == nil {
+		return nil
+	}
+	return &ApiContainerGetIndexProfileResponse_Data_UserInfo{
+		Id:              r.Id,
+		ScreenName:      r.ScreenName,
+		ProfileImageUrl: r.ProfileImageUrl,
+		ProfileUrl:      r.ProfileUrl,
+	}
+}
+
+// int64StrToInt64 转换字符串 ID 到 int64
+func int64StrToInt64(s string) int64 {
+	id, _ := strconv.ParseInt(s, 10, 64)
+	return id
 }
 
 type apiContainerGetIndexGuestCardsResponse struct {
